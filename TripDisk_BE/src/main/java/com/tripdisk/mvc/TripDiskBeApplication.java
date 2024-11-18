597f3439eb4786702f6f0d354ce746a85846b154
young5306 <harrysunae@naver.com>
2024-11-18 16:09:38 +0900

    initial commit

---
+++ TripDisk_BE/src/main/java/com/tripdisk/mvc/TripDiskBeApplication.java
@@ -0,0 +1,13 @@
+package com.tripdisk.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDiskBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripDiskBeApplication.class, args);
+	}
+
+}
