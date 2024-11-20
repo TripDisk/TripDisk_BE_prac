10e3b1ccb58d3eb0973c0eb3112b8cc1e7f0aec5
young5306 <harrysunae@naver.com>
2024-11-21 08:36:44 +0900

    cors수정

--- TripDisk_BE/src/main/java/com/tripdisk/mvc/controller/UserRestController.java
+++ TripDisk_BE/src/main/java/com/tripdisk/mvc/controller/UserRestController.java
@@ -1,9 +1,9 @@
 package com.tripdisk.mvc.controller;
 
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
